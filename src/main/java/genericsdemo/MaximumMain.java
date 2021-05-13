3592dd8bcee637ed7a60f2f80efeaf6d366b87cd
Babakailash <nathkailash2020@gmail.com>
2021-05-14 01:22:44 +0530

    Added:Refector to create generic method

--- src/main/java/genericsdemo/MaximumMain.java
+++ src/main/java/genericsdemo/MaximumMain.java
@@ -1,25 +1,6 @@
 package genericsdemo;
 
-public class MaximumMain < T extends Comparable> {
-    T x, y, z;
-
-    public MaximumMain() {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public T testMaximum(T x, T y, T z) {
-        T max = x;
-        if(y.compareTo(max) > 0) {
-            max = y;
-        }
-        if(z.compareTo(max) > 0) {
-            max = z;
-        }
-        System.out.println(" Maximum using Generic class is: " + max);
-        return max;
-    }
+public class MaximumMain {
 
     public static <T extends Comparable<T>> T printMax(T x, T y, T z) {
         T max = x;
